0a37ee11a14d65188e4647f1ee79302cb567daa1
Linda <lindatj2001@yahoo.com>
2018-01-27 01:19:37 -0800

    Test.

---
+++ GGJ18/Assets/Scripts/P1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class P1 : MonoBehaviour {
+
+    public string test = "test";
+    public int damage;
+    public int maxDamage = 10;
+    public bool isAlive = true;
+
+    // Use this for initialization
+    void Start () {
+        damage = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void UpdateDamage(int increment)
+    {
+        damage += increment;
+        checkIfAlive();
+    }
+
+    public void checkIfAlive()
+    {
+        if (damage == maxDamage)
+        {
+            isAlive = false;
+        }
+    }
+}
---
+++ GGJ18/Assets/Scripts/World.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class World : MonoBehaviour {
+
+    public GameObject player1;
+    public GameObject player2;
+
+    // Use this for initialization
+    void Start () {
+        // instantiate the first player
+        player1 = GameObject.FindWithTag("Player");
+
+        float currentRatio = (float)Screen.width / (float)Screen.height;
+
+        Vector2 vec = new Vector2(0, (float)0.25*(Screen.height/70));
+        // instantiate the second player
+        player2 = Instantiate(player1, vec, Quaternion.identity);
+        //Debug.Log(vec2);
+        /*
+        player.GetComponent<P1>().UpdateDamage(10);
+        Debug.Log("player: " + player.GetComponent<P1>().damage);
+        Debug.Log("player2: " + player2.GetComponent<P1>().damage);*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Vector2 position = Input.GetTouch(0).position;
+            Debug.Log(position);
+            ApplyDamage(position);
+            checkPlayers();
+        }
+
+        // for testing-in-Unity purposes
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 position = Input.mousePosition;
+            Debug.Log(position);
+            ApplyDamage(position);
+            checkPlayers();
+        }
+	}
+
+    void ApplyDamage(Vector2 position)
+    {
+        if (position.y > 0.5*Screen.height)
+        {
+            // attack player1
+            player1.GetComponent<P1>().UpdateDamage(1);
+            Debug.Log("player1: " + player1.GetComponent<P1>().damage);
+        }
+        else if (position.y == 0.5 * Screen.height)
+        {
+            // do nothing
+        }
+        else
+        {
+            // attack player2
+            player2.GetComponent<P1>().UpdateDamage(1);
+            Debug.Log("player2: " + player2.GetComponent<P1>().damage);
+        }
+    }
+
+    void checkPlayers()
+    {
+        if(player2.GetComponent<P1>().isAlive == false)
+        {
+            Destroy(player2);
+        }
+        if (player1.GetComponent<P1>().isAlive == false)
+        {
+            Destroy(player1);
+        }
+    }
+}
