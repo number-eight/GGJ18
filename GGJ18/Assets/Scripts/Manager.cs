7d149a0f6cd0b7e0db63f6450f29d0928f557603
Linda <lindatj2001@yahoo.com>
2018-01-27 19:35:06 -0800

    Menu setup.

---
+++ GGJ18/Assets/Scripts/Manager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour {
+
+    public void LoadScene(int level)
+    {
+        //Application.LoadLevel(level);
+        SceneManager.LoadScene(level);
+    }
+
+    void Update()
+    {
+        // close the application
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
